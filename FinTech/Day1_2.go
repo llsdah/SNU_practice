5e5c3cb0e0fefe3d92713d31e88d2d5dcc1d5a47
그냥 재미삼아 <77381506+llsdah@users.noreply.github.com>
2022-05-10 19:31:49 +0900

    Add files via upload

---
+++ FinTech/Day1_2.go
@@ -0,0 +1,25 @@
+package mypack
+
+import "fmt"
+
+//"C:\class\python\SUN_prac\Test\temp2.go"
+/*
+func main() {
+	//fmt.Println("123")
+
+	//temp2.Maps2()
+
+	fmt.Println("12312321")
+	rawLiteral := "아리랑\n 아리랑"
+	interLiteral := "리랑아릴\n"
+	fmt.Println(rawLiteral)
+	fmt.Println(interLiteral)
+	var a [1]int
+	a[0] = 1
+	//temp := make([]int, 10)
+	//temp := []int{1,2,3}
+
+	bool1, prin := odd(1)
+	fmt.Println(bool1, prin)
+}
+*/
