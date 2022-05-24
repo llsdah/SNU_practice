793cc4fd96c84f596bd7cb5f86c2b686af11d4b2
그냥 재미삼아 <77381506+llsdah@users.noreply.github.com>
2022-05-24 17:21:10 +0900

    Add files via upload

---
+++ FinTech/Day4_1.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	max := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, _ := rand.Int(rand.Reader, max)
+
+	subject := pkix.Name{
+		Organization:       []string{"test Organization"},
+		OrganizationalUnit: []string{"test"},
+		CommonName:         "GO Web Programming",
+	}
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject:      subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IPAddresses:  []net.IP{net.ParseIP("127.0.0.1")},
+	}
+
+	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
+	certOut, _ := os.Create("cert.pem")
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+
+	keyOut, _ := os.Create("key.pem")
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+	keyOut.Close()
+
+}
+
+//  서버를 보여줄 것인데 어떻게 이것으로 합니다.
+
+/*
+func (h *handler) ServerHTTP(w http.ResponseWriter, r *htttp.Request) {
+	fmt.Println(w, "hello G")
+}
+
+func main() {
+
+	handler := handler{}
+	server = http.Server{
+		Addr : "127.0.0.1:8080",
+		Handler: &handler,
+	}
+	server.ListenAndServeTLS("ert.pem")
+
+}
+
+*/
